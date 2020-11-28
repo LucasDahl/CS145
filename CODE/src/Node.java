1f980743b3e8ab00061f4cd784d7a6f14a543084
Lucas Dahl <LucasDahl425@yahoo.com>
2020-11-27 20:14:47 -0800

    Finished the Node class for the CODE assignment.

---
+++ CODE/src/Node.java
@@ -0,0 +1,74 @@
+/**
+ *  This class
+ *
+ * @author Lucas D. Dahl
+ * @version 11/24/20
+ */
+public class Node implements Comparable<Node> {
+
+    // **************************** Fields *****************************
+    private Character character;
+    private Integer frequency;
+    private Node left;
+    private Node right;
+
+    // ************************** Constructors *************************
+
+    /**
+     * This constructor sets both the character and
+     * the frequency.
+     *
+     * @param letter letter character.
+     * @param freq the frequency.
+     */
+    public Node(char letter, int freq) {
+        character = letter;
+        frequency = freq;
+    }
+
+    /**
+     * This is the default constructor.
+     */
+    public Node() {
+        character = null;
+        frequency = null;
+    }
+
+    // **************************** Methods ****************************
+
+    /**
+     * This method will return 1 if the node is greater
+     * than the compared node, and -1 if the passed in
+     * node is greater.
+     *
+     * @param node is the node to compare against.
+     * @return the int returned determines which letter is better.
+     */
+    public int compareTo(Node node) {
+        if(frequency > node.frequency || frequency == node.frequency) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Character: " + character + "Frequency: " + frequency;
+    }
+
+    /**
+     * This method will determine if
+     * the current node is a leaf or
+     * not.
+     *
+     * @return returns true if the node is a leaf.
+     */
+    public Boolean isLeaf() {
+        if (left == null && right == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
